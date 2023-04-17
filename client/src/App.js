f77eb9afa5ee2afa6f0ff5b7bc7a144c83beed72
williamxmejia <willx1995@gmail.com>
2023-04-17 17:36:51 -0500

    feat: user can select between 4 times, start, pause and reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,99 @@
-import './App.css'
-import React, { useState} from "react";
+import "./App.css";
+import React, { useState, useRef } from "react";
 
 function App() {
-  
-  const [timer, setTimer] = useState(0); // 25 minutes in seconds
+  const [timer, setTimer] = useState(1500);
+  const [selected, setSelected] = useState(1500);
+  const intervalRef = useRef(null);
 
-  const countDown = () => setInterval(() => {
-    setTimer((prevTime) => {
-      if(prevTime > 0) {
-        return prevTime - 1
-      } else {
-        return 'Expired'
-      }
-    })
-  }, 1000)
-  
+
+  const countDown = () =>
+    intervalRef.current = setInterval(() => {
+      setTimer((prevTime) => {
+        if (prevTime > 0) {
+          return prevTime - 1;
+        } else {
+          return 0;
+        }
+      });
+    }, 1000);
+
+    const handleSelected = (time) => {
+      setSelected(time)
+    }
+
+    const handleReset = (time) => {
+      setTimer(time)
+      clearInterval(intervalRef.current)
+    }
+
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes < 10 ? "0" : ""}${minutes}:${
+      seconds < 10 ? "0" : ""
+    }${seconds}`;
+  };
+
   return (
     <div className="App">
       <h1>Pomodoro Timer</h1>
-      <ul>
-        <li><button onClick={(event) => {
-          setTimer(5);
-          // console.log(event)
-          }}>15 minutes</button></li>
-        <li><button onClick={() =>setTimer(10)}>30 minutes</button></li>
-        <li><button onClick={() => setTimer(20)}>45 minutes</button></li>
-        <li><button onClick={() => setTimer(25)}>60 minutes</button></li>
-      </ul>
-      <ul>
-        <li><button onClick={countDown}>Start</button></li>
-      </ul>
-
       <div>
-        <p>{timer}</p> 
+        <p>{formatTime(timer)}</p>
       </div>
-
-
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              handleSelected(900);
+              setTimer(900);
+              handleReset(900)
+            }}
+          >
+            15 minutes
+          </button>
+        </li>
+        <li>
+          <button onClick={() => {
+            handleSelected(1800)
+            setTimer(1800)
+            handleReset(1800)
+            }}>30 minutes</button>
+        </li>
+        <li>
+          <button onClick={() => {
+            handleSelected(2700)
+            setTimer(2700)
+            handleReset(2700)
+            }}>45 minutes</button>
+        </li>
+        <li>
+          <button onClick={() => {
+            handleSelected(3600)
+            setTimer(3600)
+            handleReset(3600)
+            }}>60 minutes</button>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              clearInterval(intervalRef.current)
+              countDown()
+            }}
+          >
+            Start
+          </button>
+        </li>
+        <li>
+          <button onClick={() => clearInterval(intervalRef.current)}>Pause</button>
+        </li>
+        <li>
+          <button onClick={() => handleReset(selected)}>Reset</button>
+        </li>
+      </ul>
     </div>
   );
 }
