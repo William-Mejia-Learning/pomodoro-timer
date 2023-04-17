11796b8edd289fbaa07f63239c97bd99745e27f8
williamxmejia <willx1995@gmail.com>
2023-04-17 14:47:36 -0500

    feat:the timer counts down and stops at 0

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,38 @@
-import logo from './logo.svg'
 import './App.css'
-import { useState } from 'react'
+import React, { useState} from "react";
 
 function App() {
   
-  const [timer, setTimer] = useState('')
-  
-  let timeLeft = timer
+  const [timer, setTimer] = useState(0); // 25 minutes in seconds
 
-  const countDown = setInterval(() => {
-    timeLeft -= 1
-    setTimer(timeLeft)
-    // console.log(timeLeft)
+  const countDown = () => setInterval(() => {
+    setTimer((prevTime) => {
+      if(prevTime > 0) {
+        return prevTime - 1
+      } else {
+        return 'Expired'
+      }
+    })
   }, 1000)
   
   return (
     <div className="App">
       <h1>Pomodoro Timer</h1>
       <ul>
-        <li><button>15 minutes</button></li>
-        <li><button>30 minutes</button></li>
-        <li><button>45 minutes</button></li>
-        <li><button>60 minutes</button></li>
+        <li><button onClick={(event) => {
+          setTimer(5);
+          // console.log(event)
+          }}>15 minutes</button></li>
+        <li><button onClick={() =>setTimer(10)}>30 minutes</button></li>
+        <li><button onClick={() => setTimer(20)}>45 minutes</button></li>
+        <li><button onClick={() => setTimer(25)}>60 minutes</button></li>
+      </ul>
+      <ul>
+        <li><button onClick={countDown}>Start</button></li>
       </ul>
 
       <div>
-        {timer}
+        <p>{timer}</p> 
       </div>
 
 
