f8fbd3f1beb2f0ee810837f27a8d48b299e7b830
williamxmejia <willx1995@gmail.com>
2023-04-19 08:32:41 -0500

    feat: start pause buttons conditional, depends on if the time is running

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,75 @@
-import "./App.css";
-import React, { useState, useRef, useEffect } from "react";
+import "./App.css"
+import React, { useState, useRef, useEffect } from "react"
 
 function App() {
-  const [timer, setTimer] = useState(1500);
-  const [selected, setSelected] = useState(1500);
-  const [mode, setMode] = useState('work');
-  const [night, setNight] = useState('dark');
-  const intervalRef = useRef(null);
+  const [timer, setTimer] = useState(1500)
+  const [selected, setSelected] = useState(1500)
+  const [running, setRunning] = useState(false)
+  const [mode, setMode] = useState("work")
+  const [night, setNight] = useState("dark")
+  const intervalRef = useRef(null)
 
   const countDown = () =>
-    intervalRef.current = setInterval(() => {
+    (intervalRef.current = setInterval(() => {
       setTimer((prevTime) => {
         if (prevTime > 0) {
-          return prevTime - 1;
+          return prevTime - 1
         } else {
           play()
-          if(mode === 'work'){
-            handleMode('break')
+          if (mode === "work") {
+            handleMode("break")
             setTimer(600)
           } else {
-            handleMode('work')
+            handleMode("work")
             setTimer(selected)
           }
         }
-      });
-    }, 1000);
+      })
+    }, 1000))
 
-    const handleNight = () => {
-      if (night === 'light') {
-        setNight('dark');
-        } else {
-          setNight('light');
-        }
+  const handleNight = () => {
+    if (night === "light") {
+      setNight("dark")
+    } else {
+      setNight("light")
     }
+  }
 
-    const play = () => {
-      const audio = new Audio('./mixkit-arcade-retro-game-over-213.wav')
-      audio.play()
-    }
+  const play = () => {
+    const audio = new Audio("./mixkit-arcade-retro-game-over-213.wav")
+    audio.play()
+  }
 
-    const handleSelected = (time) => {
-      setSelected(time)
-    }
+  const handleSelected = (time) => {
+    setSelected(time)
+  }
 
-    const handleReset = (time) => {
-      setTimer(time)
-      clearInterval(intervalRef.current)
-    }
+  const handleReset = (time) => {
+    setTimer(time)
+    clearInterval(intervalRef.current)
+  }
 
-    const handleMode = (modes) => {
-      setMode(modes)
-      clearInterval(intervalRef.current)
-    }
+  const handleMode = (modes) => {
+    setMode(modes)
+    clearInterval(intervalRef.current)
+  }
 
-    useEffect(() => {
-      document.body.className = night;
-      // document.body.style.height = "100vh"
-      });
-
+  const handleRunning = (run) => {
+    setRunning(run)
+  }
 
   const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
     return `${minutes < 10 ? "0" : ""}${minutes}:${
       seconds < 10 ? "0" : ""
-    }${seconds}`;
-  };
+    }${seconds}`
+  }
+
+  useEffect(() => {
+    document.body.className = night
+  })
+
 
   return (
     <div className={`App`}>
@@ -78,68 +82,105 @@ function App() {
         <li>
           <button
             onClick={() => {
+              handleRunning(false)
               handleSelected(900);
               setTimer(900);
-              handleReset(900)
+              handleReset(900);
             }}
           >
             15 minutes
           </button>
         </li>
         <li>
-          <button onClick={() => {
-            handleSelected(1800)
-            setTimer(1800)
-            handleReset(1800)
-            }}>30 minutes</button>
-        </li>
-        <li>
-          <button onClick={() => {
-            handleSelected(2700)
-            setTimer(2700)
-            handleReset(2700)
-            }}>45 minutes</button>
-        </li>
-        <li>
-          <button onClick={() => {
-            handleSelected(3600)
-            setTimer(3600)
-            handleReset(3600)
-            }}>60 minutes</button>
-        </li>
-      </ul>
-      <ul>
-        <li>
           <button
             onClick={() => {
-              clearInterval(intervalRef.current)
-              countDown()
+              handleRunning(false)
+              handleSelected(1800)
+              setTimer(1800)
+              handleReset(1800)
             }}
           >
-            Start
+            30 minutes
           </button>
         </li>
         <li>
-          <button className="pause" onClick={() => clearInterval(intervalRef.current)}>Pause</button>
+          <button
+            onClick={() => {
+              handleRunning(false)
+              handleSelected(2700)
+              setTimer(2700)
+              handleReset(2700)
+            }}
+          >
+            45 minutes
+          </button>
         </li>
         <li>
-          <button className="reset" onClick={() => handleReset(selected)}>Reset</button>
+          <button
+            onClick={() => {
+              handleRunning(false)
+              handleSelected(3600)
+              setTimer(3600)
+              handleReset(3600)
+            }}
+          >
+            60 minutes
+          </button>
+        </li>
+      </ul>
+      <ul>
+        {(() => {
+          if (running) {
+            return (
+              <li>
+                <button
+                  className="pause"
+                  onClick={() => {
+                    handleRunning(false)
+                    clearInterval(intervalRef.current)
+                  }}
+                >
+                  Pause
+                </button>
+              </li>
+            );
+          } else {
+            return (
+              <li>
+                <button
+                  onClick={() => {
+                    handleRunning(true)
+                    clearInterval(intervalRef.current)
+                    countDown()
+                    
+                  }}
+                >
+                  Start
+                </button>
+              </li>
+            );
+          }
+        })()}
+        <li>
+          <button className="reset" onClick={() => handleReset(selected)}>
+            Reset
+          </button>
         </li>
         <li>
-          <button onClick={() => {
-            if(mode === 'work'){
-              handleMode('break')
-            } else {
-              handleMode('work')
-            }
-          }}>Mode</button>
+          <button
+            onClick={() => {
+              if (mode === "work") {
+                handleMode("break")
+              } else {
+                handleMode("work")
+              }
+            }}
+          >
+            Mode
+          </button>
         </li>
         <li>
-        <button
-        onClick={handleNight}
-      >
-        Toggle Theme
-      </button>
+          <button onClick={handleNight}>Toggle Theme</button>
         </li>
       </ul>
     </div>
